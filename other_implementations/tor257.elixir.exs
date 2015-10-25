0cb5df0ca4cc91c1017e99c0881ead8c256a2881
Arthur Poulet <arthur.poulet@cryptolab.net>
2015-10-25 12:58:00 +0100

    prepare tor257 for elixir

--- other_implementations/tor257.elixir.exs
+++ other_implementations/tor257.elixir.exs
@@ -2,22 +2,6 @@
 
 use Bitwise
 
-# TODO: read with -p and -k
-k = hd System.argv
-# TODO: read by 4096 on stdin
-m = hd tl System.argv
-
-# TODO: rm debug message
-IO.puts "Message: #{m}"
-IO.puts "Key    : #{k}"
-
-# TODO: check for performances
-# Check for the useful type to use (to avoid heavy cast)
-k = String.to_char_list k
-k_size = Enum.count k
-k = List.to_tuple k
-m = String.to_char_list m
-
 defmodule Tor257 do
   def rol8(b, nb_bits) do
     high = b <<< (rem(nb_bits, 8))
@@ -64,10 +48,34 @@ defmodule Tor257 do
 			{2} -> 3
 		end
 		current = [elem(key, (rem idx, ksize))]
-		IO.puts("Current: #{subkey}, #{current}, #{idx}")
 		subkeys(key, subkey ++ current, idx + offset, used + offset, new_offset, ksize)
 	end
 end
+
+# TODO: read with -p and -k
+k = hd System.argv
+# TODO: read by 4096 on stdin
+m = hd tl System.argv
+
+# TODO: rm debug message
+IO.puts "Message: #{m}"
+IO.puts "Key    : #{k}"
+
+# TODO: check for performances
+# Check for the useful type to use (to avoid heavy cast)
+k = String.to_char_list k
+k_size = Enum.count k
+k = List.to_tuple k
+m = String.to_char_list m
+
+klist = Tuple.to_list(k)
+subkeys = Enum.map(Enum.with_index(klist), fn char_with_index ->
+  idx = elem char_with_index, 1
+  char = elem char_with_index, 0
+	sk = Tor257.subkeys(k, idx, Enum.count(klist))
+	IO.puts "Key: create subkey #{idx}: #{sk}"
+	List.to_tuple(sk)
+end)
 
 Enum.map(Enum.with_index(m), fn char_with_index ->
   idx = elem char_with_index, 1
@@ -78,12 +86,4 @@ Enum.map(Enum.with_index(m), fn char_with_index ->
   encrypted = Tor257.execute(char, kbyte, offsets)
   # TODO: remove debug
   IO.puts "#{idx} => #{char} xor #{kbyte} = #{encrypted}"
-end)
-
-klist = Tuple.to_list(k)
-Enum.map(Enum.with_index(klist), fn char_with_index ->
-  idx = elem char_with_index, 1
-  char = elem char_with_index, 0
-	sk = Tor257.subkeys(k, idx, Enum.count(klist))
-	IO.puts "Key: create subkey #{idx}: #{sk}"
 end)
